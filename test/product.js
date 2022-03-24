7cdf3434d49c38dd52533857d2c09af84e444150
Simon B. Carstensen <simonbc@gmail.com>
2022-03-24 21:26:13 +0100

    Update test cases

--- test/product.js
+++ test/product.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 
 const ProductArtifact = require("../artifacts/contracts/ProductV2.sol/Product.json");
 
-describe("Product", () => {
+describe("Products", () => {
   let Product, product, addr1, addr2, addr3, addr4, addr5;
 
   beforeEach(async () => {
@@ -12,15 +12,14 @@ describe("Product", () => {
     productFactory = await ProductFactory.deploy();
   });
 
-  describe("Products", () => {
+  describe("Create products", () => {
     it("Should create product", async () => {
-      const name = "Test product";
-      const baseUri = "https://ipfs.infura.io/ipfs/";
+      const tokenUri = "https://ipfs.infura.io/ipfs/";
       const slug = "test";
       const seller = owner.address;
       const price = 1;
-      const amount = 10;
-      await productFactory.create(name, baseUri, slug, seller, price, amount);
+      const supply = 10;
+      await productFactory.create(tokenUri, slug, seller, price, supply);
 
       const productAddrs = await productFactory.fetchProducts();
       expect(productAddrs.length).to.equal(1);
@@ -30,24 +29,21 @@ describe("Product", () => {
         ProductArtifact.abi,
         owner
       );
-      expect(await product.name()).to.equal(name);
-      expect(await product.baseUri()).to.equal(baseUri);
-      expect(await product.slug()).to.equal(slug);
+      expect(await product.tokenUri()).to.equal(tokenUri);
       expect(await product.price()).to.equal(price);
-      expect(await product.amount()).to.equal(amount);
+      expect(await product.supply()).to.equal(supply);
     });
 
-    it("Should not create product unless amount > 0", async () => {
+    it("Should not create product unless supply > 0", async () => {
       await expect(
         productFactory.create(
-          "Test product",
           "https://ipfs.infura.io/ipfs/",
           "test",
           owner.address,
           1,
           0
         )
-      ).to.be.revertedWith("Required to mint at least 1 product");
+      ).to.be.revertedWith("Error, supply must be at least 1");
     });
 
     it("Should fetch all seller products", async () => {
@@ -56,7 +52,6 @@ describe("Product", () => {
 
       // Create product with owner address
       await productFactory.create(
-        "Test product",
         "https://ipfs.infura.io/ipfs/",
         "test",
         owner.address,
@@ -68,7 +63,6 @@ describe("Product", () => {
 
       // // Create product with addr1 address
       await productFactory.create(
-        "Test product",
         "https://ipfs.infura.io/ipfs/",
         "test",
         addr1.address,
@@ -80,33 +74,65 @@ describe("Product", () => {
     });
   });
 
-  describe("Mint product", () => {
-    it("Should mint product", async () => {
+  describe("Fetch products", () => {
+    it("Should fetch product by seller", async () => {
       await productFactory.create(
-        "Test product",
         "https://ipfs.infura.io/ipfs/",
-        "test",
+        "test1",
         owner.address,
         1,
         10
       );
-      const [productAddr] = await productFactory.fetchProducts();
-      const product = new ethers.Contract(
-        productAddr,
-        ProductArtifact.abi,
-        owner
+      products = await productFactory.fetchSellerProducts(owner.address);
+      expect(products.length).to.equal(1);
+    });
+
+    it("Should fetch product by slug", async () => {
+      let product = await productFactory.fetchProductBySlug(
+        addr1.address,
+        "test"
+      );
+      expect(product).to.equal("0x0000000000000000000000000000000000000000");
+
+      // Create product with owner address
+      await productFactory.create(
+        "https://ipfs.infura.io/ipfs/",
+        "test1",
+        owner.address,
+        1,
+        10
+      );
+      products = product = await productFactory.fetchProductBySlug(
+        addr1.address,
+        "test1"
+      );
+      expect(product).to.equal("0x0000000000000000000000000000000000000000");
+
+      products = product = await productFactory.fetchProductBySlug(
+        owner.address,
+        "test"
+      );
+      expect(product).to.equal("0x0000000000000000000000000000000000000000");
+
+      // // Create product with addr1 address
+      await productFactory.create(
+        "https://ipfs.infura.io/ipfs/",
+        "test",
+        addr1.address,
+        1,
+        10
       );
 
-      await product.mint(1, "1234");
-      const tokenUri = await product.uri(1);
-      expect(tokenUri).to.equal("https://ipfs.infura.io/ipfs/1234.json");
+      product = await productFactory.fetchProductBySlug(addr1.address, "test");
+      expect(product).to.not.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
     });
   });
 
   describe("Buy product", () => {
     it("Should buy product", async () => {
       await productFactory.create(
-        "Test product",
         "https://ipfs.infura.io/ipfs/",
         "test",
         owner.address,
@@ -119,8 +145,7 @@ describe("Product", () => {
         ProductArtifact.abi,
         owner
       );
-      await product.mint(10, "1234");
-      await product.connect(addr1).buy(1, 2, {
+      await product.connect(addr1).buy(2, {
         value: 2,
       });
       expect(await product.sold()).to.equal(2);
@@ -129,7 +154,6 @@ describe("Product", () => {
 
   it("Should not buy product if msg.value too low", async () => {
     await productFactory.create(
-      "Test product",
       "https://ipfs.infura.io/ipfs/",
       "test",
       owner.address,
@@ -142,22 +166,20 @@ describe("Product", () => {
       ProductArtifact.abi,
       owner
     );
-    await product.mint(10, "1234");
     await expect(
-      product.connect(addr1).buy(1, 2, {
+      product.connect(addr1).buy(2, {
         value: 1,
       })
     ).to.be.revertedWith("Error, product costs more");
   });
 
-  it("Should not buy product, if insufficient balance", async () => {
+  it("Should not buy product, if insufficient supply", async () => {
     await productFactory.create(
-      "Test product",
       "https://ipfs.infura.io/ipfs/",
       "test",
       owner.address,
       1,
-      10
+      1
     );
     const [productAddr] = await productFactory.fetchProducts();
     const product = new ethers.Contract(
@@ -165,11 +187,10 @@ describe("Product", () => {
       ProductArtifact.abi,
       owner
     );
-    await product.mint(1, "1234");
     await expect(
-      product.connect(addr1).buy(1, 2, {
+      product.connect(addr1).buy(2, {
         value: 2,
       })
-    ).to.be.revertedWith("ERC1155: insufficient balance for transfer");
+    ).to.be.revertedWith("Error, amount is higher than available supply");
   });
 });
