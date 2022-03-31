6f7a78db7693059670ff5c1ea4f2149458628f7e
Simon B. Carstensen <simonbc@gmail.com>
2022-03-31 18:22:36 +0200

    Remove unused endpoint

--- app/pages/api/[chainId]/products/index.js
+++
@@ -1,5 +0,0 @@
-import { collection, getDocs, addDoc, query, where } from "firebase/firestore";
-
-import { database } from "../../../firebaseConfig";
-
-export default function handler(req, res) {}
